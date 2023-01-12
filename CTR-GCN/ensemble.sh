0a85255caedc90ee0195aa4e0edaeb5c9e2526b0
K. Prasse <katharina.prasse@posteo.de>
2023-01-12 10:41:19 +0100

    include CTR-GCN model in the output

--- CTR-GCN/ensemble.sh
+++ CTR-GCN/ensemble.sh
@@ -1,14 +1,34 @@
+### Baseline
+# ## XSUB
+
+# python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHT --bone-dir work_dir/csub/local_SHT_bone
+# python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHT_BL --bone-dir work_dir/csub/local_SHT_bone_BL
+
+# python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHT --bone-dir work_dir/csub/local_SHT_bone --joint-motion-dir work_dir/csub/local_SHT_vel --bone-motion-dir work_dir/csub/local_SHT_bonevel
+# python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHT_BL --bone-dir work_dir/csub/local_SHT_bone_BL --joint-motion-dir work_dir/csub/local_SHT_vel_BL --bone-motion-dir work_dir/csub/local_SHT_bonevel_BL
+
+# ## XSET
+# python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHT --bone-dir work_dir/cset/local_SHT_bone
+# python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHT_BL --bone-dir work_dir/cset/local_SHT_bone_BL
+
+# python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHT --bone-dir work_dir/cset/local_SHT_bone --joint-motion-dir work_dir/cset/local_SHT_vel --bone-motion-dir work_dir/cset/local_SHT_bonevel
+# python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHT_BL --bone-dir work_dir/cset/local_SHT_bone_BL --joint-motion-dir work_dir/cset/local_SHT_vel_BL --bone-motion-dir work_dir/cset/local_SHT_bonevel_BL
+
+
+### CTRGCN
 ## XSUB
+Write_Output "XSUB"
+python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/ctrgcn_local_SHT --bone-dir work_dir/csub/ctrgcn_local_SHT_bone
+python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/ctrgcn_local_SHT_BL --bone-dir work_dir/csub/ctrgcn_local_SHT_bone_BL
 
-python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHTg --bone-dir work_dir/csub/local_SHTg_bone
-python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHTg_BL --bone-dir work_dir/csub/local_SHTg_bone_BL
-
-python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHTg --bone-dir work_dir/csub/local_SHTg_bone --joint-motion-dir work_dir/csub/local_SHTg_vel --bone-motion-dir work_dir/csub/local_SHTg_bonevel
-python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/local_SHTg_BL --bone-dir work_dir/csub/local_SHTg_bone_BL --joint-motion-dir work_dir/csub/local_SHTg_vel_BL --bone-motion-dir work_dir/csub/local_SHTg_bonevel_BL
+python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/ctrgcn_local_SHT --bone-dir work_dir/csub/ctrgcn_local_SHT_bone --joint-motion-dir work_dir/csub/ctrgcn_local_SHT_vel --bone-motion-dir work_dir/csub/ctrgcn_local_SHT_bonevel
+python ensemble.py --dataset ntu120/xsub --joint-dir work_dir/csub/ctrgcn_local_SHT_BL --bone-dir work_dir/csub/ctrgcn_local_SHT_bone_BL --joint-motion-dir work_dir/csub/ctrgcn_local_SHT_vel_BL --bone-motion-dir work_dir/csub/ctrgcn_local_SHT_bonevel_BL
 
 ## XSET
-python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHTg --bone-dir work_dir/cset/local_SHTg_bone
-python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHTg_BL --bone-dir work_dir/cset/local_SHTg_bone_BL
+Write_Output "XSET" | Get-Member
+python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/ctrgcn_local_SHT --bone-dir work_dir/cset/ctrgcn_local_SHT_bone
+python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/ctrgcn_local_SHT_BL --bone-dir work_dir/cset/ctrgcn_local_SHT_bone_BL
 
-python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHTg --bone-dir work_dir/cset/local_SHTg_bone
-python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/local_SHTg_BL --bone-dir work_dir/cset/local_SHTg_bone_BL --joint-motion-dir work_dir/cset/local_SHTg_vel_BL --bone-motion-dir work_dir/cset/local_SHTg_bonevel_BL
+python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/ctrgcn_local_SHT --bone-dir work_dir/cset/ctrgcn_local_SHT_bone --joint-motion-dir work_dir/cset/ctrgcn_local_SHT_vel --bone-motion-dir work_dir/cset/ctrgcn_local_SHT_bonevel
+python ensemble.py --dataset ntu120/xset --joint-dir work_dir/cset/ctrgcn_local_SHT_BL --bone-dir work_dir/cset/ctrgcn_local_SHT_bone_BL --joint-motion-dir work_dir/cset/ctrgcn_local_SHT_vel_BL --bone-motion-dir work_dir/cset/ctrgcn_local_SHT_bonevel_BL
+
